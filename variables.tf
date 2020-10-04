d8e171e5a30346800a7a67b96f232668f35e5cb4
Michael Simo <mpmsimo@gmail.com>
2020-10-03 22:53:08 -0700

    update username var

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 // Common variables associated with the bootstrap template
 locals {
-  organization = "ProjektDragon" // Terraform Cloud + GitHub Organization name
-  service_url = "http://projektdragon.com"
-  approved_users         = ["mpmsimo"]
-  terraform_version      = "0.12.24"
+  organization      = "ProjektDragon" // Terraform Cloud + GitHub Organization name
+  service_url       = "http://projektdragon.com"
+  approved_users    = ["reulan"]
+  terraform_version = "0.12.24"
 
 }
 
